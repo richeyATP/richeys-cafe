22949e4245a4287aed53c783aa051bf0757c6d7d
richeyATP <richey@avenirtechpartners.com>
2023-04-11 06:35:25 +0000

    force migration

--- coderscafe/customer/models.py
+++ coderscafe/customer/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
+  
     description = models.TextField()
     image = models.ImageField(upload_to='menu_images/')
     price = models.DecimalField(max_digits=5, decimal_places=2)

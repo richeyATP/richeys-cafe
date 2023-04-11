c1e50bed6bb1e069a65dc61516b56b3bec1bc780
richeyATP <richey@avenirtechpartners.com>
2023-04-11 04:51:44 +0000

    final

--- coderscafe/coderscafe/settings.py
+++ coderscafe/coderscafe/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#4)+0crrh8f*if)_vaq$mifug*8v8ui(j)0ty899ljzaw&$je7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.fly.dev']  # <-- Updated!
 

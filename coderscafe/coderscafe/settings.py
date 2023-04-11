d738f4ed63c44f35c16812b7d034cec98a694916
richeyATP <richey@avenirtechpartners.com>
2023-04-11 01:26:32 +0000

    statc redirect

--- coderscafe/coderscafe/settings.py
+++ coderscafe/coderscafe/settings.py
@@ -145,6 +145,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 

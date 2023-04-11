b5342612b7f52e49c45406d3542759c3a982f46c
richeyATP <richey@avenirtechpartners.com>
2023-04-11 01:47:11 +0000

    deployment attempt 2

--- coderscafe/coderscafe/settings.py
+++ coderscafe/coderscafe/settings.py
@@ -145,10 +145,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
